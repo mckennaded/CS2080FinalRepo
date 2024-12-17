905652025617a0fc315185d5cb9d1e2cb4bee7dd
Mckenna <mdedric2@uccs.edu>
2024-12-17 15:45:06 +0000

    dockerfile

--- TemplateDocker/testScript.sh
+++ TemplateDocker/testScript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-service nginx start
+#Copyright(c) Mckenna Dedrick
+#12/15/24
 
 sed -i 's/Welcome to nginx/Welcome to Mckenna\'\''s page/g' /var/www/html/*.html
 
 sed -i 's/nginx/nginx (pronounced as EngineX)/g' /var/www/html/*.html
 
+nginx -g 'daemon off;'
 
 tail -f /var/log/nginx/access.log
