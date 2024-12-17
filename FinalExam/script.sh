26993bd3702e14d4f884393073208a8db0374f79
Mckenna <mdedric2@uccs.edu>
2024-12-17 18:32:20 +0000

    changes

--- FinalExam/script.sh
+++ FinalExam/script.sh
@@ -5,12 +5,13 @@
 
 service nginx start
 
-sed -i 's/Welcome to nginx/Welcome to Mckenna\'\''s page/g' /var/www/html/*.html
 
-sed -i 's/nginx/nginx (pronounced as EngineX)/g' /var/www/html/*.html
+sudo sed -i 's/Welcome to nginx!/Welcome to Mckenna'\''s page/g' /var/www/html/*.html
+
+sudo sed -i 's/nginx/nginx (pronounced as EngineX)/g' /var/www/html/*.html
 
 service nginx stop
 
 service nginx start
 
-tail -f /var/log/nginx/access.log
+
