378c32ee1e892a50d0569bafdd1ca94ed1cc2e5f
Mckenna <mdedric2@uccs.edu>
2024-12-17 17:11:35 +0000

    Dockerfile

--- FinalExam/script.sh
+++ FinalExam/script.sh
@@ -3,10 +3,14 @@
 #Copyright (c) Mckenna Dedrick 12/17/24
 #
 
+service nginx start
+
 sed -i 's/Welcome to nginx/Welcome to Mckenna\'\''s page/g' /var/www/html/*.html
 
 sed -i 's/nginx/nginx (pronounced as EngineX)/g' /var/www/html/*.html
 
-nginx -g 'daemon off;'
+service nginx stop
+
+service nginx start
 
 tail -f /var/log/nginx/access.log
